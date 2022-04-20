44a4cd18a7425de1649d1dd7ffa35430a1fababb
Gustavo Rodrigues <gustavo.rodrigues@sequor.com.br>
2022-04-20 17:00:36 -0300

    feat:inclusion-of-classes

--- LanchesMac/Migrations/20220401132012_PedidoDetalhes.cs
+++ LanchesMac/Migrations/20220401132012_PedidoDetalhes.cs
@@ -9,13 +9,14 @@ namespace LanchesMac.Migrations
         {
             migrationBuilder.CreateTable(
                 name: "Pedidos",
-                columns: table => new {
+                columns: table => new 
+                {
                     PedidoId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1,1"),
                     Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    Sobrenome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Sobrenome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
                     Endereco1 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    Endereco2 = table.Column<string>(type: "nvarchar(100", maxLength: 50, nullable: true),
+                    Endereco2 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
                     Cep = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
                     Estado = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
                     Cidade = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
@@ -25,13 +26,13 @@ namespace LanchesMac.Migrations
                     TotalItensPedido = table.Column<int>(type: "int", nullable: false),
                     PedidoEnviado = table.Column<DateTime>(type: "datetime2", nullable: false),
                     PedidoEntregueEm = table.Column<DateTime>(type: "datetime2", nullable: true)
-
                 },
-                constraints: table => {
+                constraints: table => 
+                {
                     table.PrimaryKey("PK_Pedidos", x => x.PedidoId);
                 });
 
-            migrationBuilder.CreateTable(
+                migrationBuilder.CreateTable(
                 name: "PedidoDetalhes",
                 columns: table => new {
                     PedidoDetalheId = table.Column<int>(type: "int", nullable: false)
@@ -39,7 +40,7 @@ namespace LanchesMac.Migrations
                     PedidoId = table.Column<int>(type: "int", nullable: false),
                     LancheId = table.Column<int>(type: "int", nullable: false),
                     Quantidade = table.Column<int>(type: "int", nullable: false),
-                    Preco = table.Column<decimal>(type: "decimal", nullable: false)
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
                 },
                 constraints: table => {
                     table.PrimaryKey("PK_PedidoDetalhes", x => x.PedidoDetalheId);
@@ -50,7 +51,7 @@ namespace LanchesMac.Migrations
                         principalColumn: "LanchesId",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
-                        name: "FK_PedidoDetalhes_PedidoId",
+                        name: "FK_PedidoDetalhes_Pedidos_PedidoId",
                         column: x => x.PedidoId,
                         principalTable: "Pedidos",
                         principalColumn: "PedidoId",
@@ -63,7 +64,7 @@ namespace LanchesMac.Migrations
                 column: "LancheId");
 
             migrationBuilder.CreateIndex(
-                name: "IX_PedidoDetalhes_LancheId",
+                name: "IX_PedidoDetalhes_PedidoId",
                 table: "PedidoDetalhes",
                 column: "PedidoId");
         }
---
+++ LanchesMac/Models/Pedido.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Xunit;
+
+namespace LanchesMac.Models {
+    public class Pedido {
+        public int PedidoID { get; set; }
+        [Required(ErrorMessage = "Informe o nome")]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe sobrenome")]
+        [StringLength(50)]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Informe seu endereço")]
+        [StringLength(100)]
+        [Display(Name = "Endereço")]
+
+        public string Endereco1 { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Complemento")]
+
+        public string Endereco2 { get; set; }
+
+        [Required(ErrorMessage = "Informe o seu CEP")]
+        [Display(Name = "CEP")]
+        [StringLength(10, MinimumLength = 8)]
+        public string Cep { get; set; }
+
+        [StringLength(10)]
+        public string Estado { get; set; }
+
+        [StringLength(50)]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Informe o seu telefone")]
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
+
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Informe o email")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?: [a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/",
+            ErrorMessage = "O email não possui um formato correto")]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Column(TypeName = "decimal(18,2")]
+        [Display(Name = "Total do Pedido")]
+        public decimal PedidoTotal { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Display(Name = "Itens no Pedido")]
+        public int TotalItensPedido { get; set; }
+
+        [Display(Name = "Data do Pedido")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy hh:mm}")]
+        public DateTime PedidoEnviado { get; set; }
+
+        [Display(Name = "Data Envio Pedido")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy hh:mm")]
+
+        public DateTime? PedidoEntregueEm { get; set; }
+
+        public List<PedidoDetalhe> PedidoItens { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
